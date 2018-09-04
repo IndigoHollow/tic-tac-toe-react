04a2959d265ff8160271e56850be12c090f89cb1
Sergey Gubarev <celtic_moon@mail.ru>
2018-09-04 12:36:24 +0300

    Update index.js
    Fixed some issues

--- js/index.js
+++ js/index.js
@@ -12,7 +12,7 @@ class Board extends React.Component {
   constructor(props) {
     super(props);
     
-    //this.handleClick = this.handleClick.bind(this);
+    this.handleClick = this.handleClick.bind(this);
     
     this.state = {
       squares: Array(9).fill(null),
@@ -75,15 +75,10 @@ class Game extends React.Component {
         <div className="game-board">
           <Board />
         </div>
-        <div className="game-info">
-          <div>{/* status */}</div>
-          <ol>{/* TODO */}</ol>
-        </div>
       </div>
     );
   }
 }
-
 
 ReactDOM.render(
   <Game />,
